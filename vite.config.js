55a6bb591935b920f2b6efb634966dfd70dab34e
hiteshdotcom <hiteshpal.8097@gmail.com>
2024-06-23 12:42:36 +0530

    add build properties in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from 'path'
 
 console.log("Load env ", loadEnv("dev", "./"));
 // https://vitejs.dev/config/
@@ -11,6 +12,14 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          nested: resolve(__dirname, "nested/index.html"),
+        },
+      },
+    },
   });
 };
 // export default defineConfig({
