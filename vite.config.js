8c271cf723ab2cf767375703d3e65acf6e2fdb8f
hiteshdotcom <hiteshpal.8097@gmail.com>
2024-06-23 12:43:42 +0530

    minor bug resole of nested index file in vite config

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,6 @@ export default ({ mode }) => {
       rollupOptions: {
         input: {
           main: resolve(__dirname, "index.html"),
-          nested: resolve(__dirname, "nested/index.html"),
         },
       },
     },
